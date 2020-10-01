1aa03a1af938bbccf733d9859bf102801172808e
danozz86 <danozzy86@gmail.com>
2020-10-01 15:04:26 -0400

    dlo

---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+/* 
+   Lab 4 - String Operations on a File - Programming 1
+   Daniel Osborne Github - danozzy86
+   Due 10/1 @ 4PM
+*/
+namespace Lab4
+{
+    class Program
+    {
+        public static bool exit = false;
+        static void Main(string[] args)
+        {
+            while (!exit)
+            {
+                MainMenu();
+
+                string choiceString = Console.ReadLine();
+                int intChoice;
+                string txtFile;
+
+                //Verifies the user input to make sure that they are inputing a number integer option.3
+                while (!int.TryParse(choiceString, out intChoice))
+                {
+                    Console.Clear();
+                    MainMenu();
+                    Console.WriteLine("Please enter a valid number.");
+                    choiceString = Console.ReadLine();
+                }
+
+                switch (intChoice)
+                {
+                    case 1:
+                        //Retrieves the text file input from the ReadFile method and puts it in the txtFile string.
+                        txtFile = ReadFile();
+
+                        if (txtFile != null)
+                        {
+                            Console.Clear();
+                            Console.WriteLine(txtFile);
+                            Console.WriteLine("The text file has {0} words.", StringOperators.WordCount(txtFile));
+                            Console.WriteLine("The text file has {0} vowels", StringOperators.CharCount(txtFile, true));
+                            Console.WriteLine("The text file has {0} letters", StringOperators.CharCount(txtFile, false));
+                            Console.WriteLine("Press any key to continue...");
+                            Console.ReadKey();
+                        }
+                        break;
+                    case 2:
+                        //Creates an empty text file in user designated path and validates the path
+                        CreateFile();
+                        break;
+                    case 3:
+                        //Exits the program
+                        Console.Clear();
+                        exit = true;
+                        break;
+                    default:
+                        //By default, will return to the beginingof the loop, most likely due to incorrect option being selected.
+                        Console.Clear();
+                        Console.WriteLine("Please enter a valid choice of 1, 2, or 3.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+        public static string ReadFile()
+        {
+            Console.Clear();
+            Console.WriteLine("=========================================");
+            Console.WriteLine("            Read a Text File             ");
+            Console.WriteLine("=========================================");
+            Console.WriteLine("- Enter the File Path to the Text File - ");
+
+            string path = Console.ReadLine();
+            string input;
+
+            try
+            {
+                input = File.ReadAllText(path);
+            }
+            catch
+            {
+                Console.WriteLine("The file could not be read or does not exist.");
+                Console.ReadKey();
+                return null;
+            }
+            return input;
+        }
+        public static void CreateFile()
+        {
+            Console.Clear();
+            Console.WriteLine("=========================================");
+            Console.WriteLine("           Create a Text File            ");
+            Console.WriteLine("=========================================");
+            Console.WriteLine("- Enter where you want the file to be located w/ name -");
+            Console.WriteLine(@"- Example: c:\users\user\desktop\example.txt");
+
+            string path = Convert.ToString(Console.ReadLine());
+
+            if (!File.Exists(path))
+            {
+                try {
+                    File.CreateText(path);
+                } catch {
+                    Console.WriteLine("The chosen file path is invalid.");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    return;
+                }
+                Console.WriteLine("The file has been created!");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Either the file already exists.");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+        public static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("=========================================");
+            Console.WriteLine(" Reading a Text File and Displaying Info ");
+            Console.WriteLine("=========================================");
+            Console.WriteLine("- Select an Option -");
+            Console.WriteLine(" 1) Read a Text File");
+            Console.WriteLine(" 2) Create a Text File");
+            Console.WriteLine(" 3) Exit Program");
+        }
+    }
+}
---
+++ StringOperators.cs
@@ -0,0 +1,69 @@
+using System;
+/* 
+   Lab 4 - String Operations on a File - Programming 1
+   Daniel Osborne Github - danozzy86
+   Due 10/1 @ 4PM
+*/
+class StringOperators
+{
+    public static int CharCount(string usrInput, bool vowel)
+    {
+        //Loops through and checks how many vowels or letters there are in the string by using the split method.
+        char[] vowelArr = { 'A', 'E', 'I', 'O', 'U' };
+        char[] letterArr = {'A','B','C','D','E','F','G','H','I','J','K','L',
+                            'M','N','O','P','Q','R','S','T','U','V','W','X',
+                            'Y','Z'};
+
+        usrInput = usrInput.ToUpper();
+        int intCharCount = 0;
+
+        if (vowel)
+        {
+            //Counts the amount of vowels present in the string.
+            foreach (char v in vowelArr)
+            {
+                intCharCount += usrInput.Split(v).Length;
+            }
+            return intCharCount - vowelArr.Length;
+        }
+        else
+        {
+            //Counts the amount of alphabetic characters in the string.
+            foreach (char v in letterArr)
+            {
+                intCharCount += usrInput.Split(v).Length;
+            }
+            return intCharCount - letterArr.Length;
+        }
+    }
+    public static int WordCount(string usrInput)
+    {
+        //This method is still a WIP, basically just checks for spaces to count words. Also now uses line breaks.
+        int intWordCount = 1;
+        int strPos = 0;
+
+        while (strPos <= usrInput.Length - 1)
+        {
+            if (usrInput[strPos] == ' ' || usrInput[strPos] == '\n')
+            {
+                intWordCount++;
+            }
+            strPos++;
+        }
+        return intWordCount;
+    }
+    public static bool IsPalindrome(string usrInput)
+    {
+        //This method will basically seperate the string into a char array, reverse it, turn it back into a string then compare.
+        string usrInputRev;
+        char[] charArr = usrInput.ToCharArray();
+
+        Array.Reverse(charArr);
+        usrInputRev = new string(charArr);
+
+        if (usrInput == usrInputRev)
+            return true;
+        else
+            return false;
+    }
+}
